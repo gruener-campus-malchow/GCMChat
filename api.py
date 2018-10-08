65ca91cc452eaa831549560fefbb7bab66b15188
sn0wmanmj <sn0wmanmj@users.noreply.github.com>
2018-10-08 20:52:15 +0200

    initial db implementation

--- api.py
+++ api.py
@@ -1,9 +1,16 @@
 from flask import Flask
 from flask_restful import Api, Resource, reqparse
+import sqlite3
 
 app = Flask(__name__)
 api = Api(app)
 
+db = sqlite3.connect('gcmchatdata.db', check_same_thread=False)
+c = db.cursor()
+
+c.execute(
+    '''CREATE TABLE IF NOT EXISTS messages(id INT PRIMARY KEY, username VARCHAR(64), text VARCHAR(255), UNIQUE(id))''')
+db.commit()
 
 class ChatAPI(Resource):
     def __init__(self):
@@ -13,16 +20,18 @@ class ChatAPI(Resource):
         super(ChatAPI, self).__init__()
 
     def get(self):
-        return [
-            {
-                "username": "testuser1",
-                "text": "text1"
-            },
-            {
-                "username": "testuser2",
-                "text": "text2"
-            }
-        ]
+        c.execute('''SELECT username, text FROM messages''')
+        return c.fetchall()
+        # return [
+        #     {
+        #         "username": "testuser1",
+        #         "text": "text1"
+        #     },
+        #     {
+        #         "username": "testuser2",
+        #         "text": "text2"
+        #     }
+        # ]
 
     def post(self):
         # print(self.reqparse.parse_args())
