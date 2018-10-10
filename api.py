5c633fc0006c608ac124a695ae977f70d5d6a242
sn0wmanmj <sn0wmanmj@users.noreply.github.com>
2018-10-10 18:02:45 +0200

    remove sqlite3

--- api.py
+++ api.py
@@ -1,18 +1,10 @@
 from flask import Flask
 from flask_restful import Api, Resource, reqparse
 from flask_cors import CORS
-import sqlite3
 
 app = Flask(__name__)
 CORS(app)
 api = Api(app)
-
-db = sqlite3.connect('gcmchatdata.db', check_same_thread=False)
-c = db.cursor()
-
-c.execute(
-    '''CREATE TABLE IF NOT EXISTS messages(id INT PRIMARY KEY, username VARCHAR(64), text VARCHAR(255), UNIQUE(id))''')
-db.commit()
 
 
 class ChatAPI(Resource):
@@ -23,18 +15,16 @@ class ChatAPI(Resource):
         super(ChatAPI, self).__init__()
 
     def get(self):
-        c.execute('''SELECT username, text FROM messages''')
-        return c.fetchall()
-        # return [
-        #     {
-        #         "username": "testuser1",
-        #         "text": "text1"
-        #     },
-        #     {
-        #         "username": "testuser2",
-        #         "text": "text2"
-        #     }
-        # ]
+        return [
+            {
+                "username": "testuser1",
+                "text": "text1"
+            },
+            {
+                "username": "testuser2",
+                "text": "text2"
+            }
+        ]
 
     def post(self):
         # print(self.reqparse.parse_args())
