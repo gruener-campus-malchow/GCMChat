b9fc40e0f61387def494aeaa97e079ec92345e28
FreeSoccerHDX <FreeSoccerHDX@gmail.com>
2018-10-10 19:55:35 +0200

    Javascript ideen hinzugefügt

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,10 +7,11 @@ function checkLoginName(name){
 	var search = /[^0-9,a-z,A-Z]/g;
 	return name.match(search);
 }
+var username;
 
 function login(){
 	let xhr = new XMLHttpRequest;
-	var username = document.getElementById("username").value;
+	username = document.getElementById("username").value;
 	var checkedusername = checkLoginName(username);
 	if(checkedusername == null){
 		xhr.open('GET', 'https://sn0wman.pythonanywhere.com/api?username=' + username, true);
@@ -20,9 +21,11 @@ function login(){
 				for(var i in liste){
 					console.log(liste[i])
 					for(var x in liste[i]){
-						if(x == "status"){
+						if(x == "status"){ //sollte unterschiedliche sachen ergeben; zB falsches PW, Username exestiert nicht, sonstiges
 							var status = liste[i][x];
-							document.getElementById("p1").innerHTML = status;
+							document.body.innerHTML = status; 
+							// evlt. die Chat-Seite in Js einbringen und hier laden, so bleibt der token erhalten 
+							
 						}
 					}
 				}
