9439dc43f74986a6c4b71b943daa86af5a0e8d8f
sn0wmanmj <sn0wmanmj@users.noreply.github.com>
2018-11-06 19:28:29 +0100

    added endpoints for registering a username and logging in

--- api.py
+++ api.py
@@ -1,13 +1,15 @@
+import bcrypt
 from flask import Flask
-from flask_restful import Api, Resource, reqparse
 from flask_cors import CORS
-from tinydb import TinyDB
+from flask_restful import Api, Resource, reqparse
+from tinydb import TinyDB, Query
 
 app = Flask(__name__)
 CORS(app)
 api = Api(app)
 
-db = TinyDB('chatdb.json')
+chatdb = TinyDB('chatdb.json')
+userdb = TinyDB('userdb.json')
 
 
 class ChatAPI(Resource):
@@ -18,21 +20,61 @@ class ChatAPI(Resource):
         super(ChatAPI, self).__init__()
 
     def get(self):
-        return db.all()
+        return chatdb.all()
 
     def post(self):
-        db.insert(self.reqparse.parse_args())
+        chatdb.insert(self.reqparse.parse_args())
         return "Success"
 
 
 class DataPurge(Resource):
     def get(self):
-        db.purge()
+        chatdb.purge()
         return "Data purged"
+
+
+class Register(Resource):
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('username', type=str)
+        self.reqparse.add_argument('password', type=str)
+        super(Register, self).__init__()
+
+    def post(self):
+        username = self.reqparse.parse_args()['username']
+
+        ExistiertBenutzer = Query()
+        if not userdb.search(ExistiertBenutzer.username == username):
+            password = self.reqparse.parse_args()['password']
+            password_hashed = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+            userdb.insert({'username': username, 'passwordhash': password_hashed.decode()})
+            return "Success"
+        else:
+            return "ExistingUsername"
+
+
+class Login(Resource):
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('username', type=str)
+        self.reqparse.add_argument('password', type=str)
+        super(Login, self).__init__()
+
+    def post(self):
+        password = self.reqparse.parse_args()['password']
+        username = self.reqparse.parse_args()['username']
+        UserInDB = Query()
+        password_hashed = userdb.search(UserInDB.username == username)[0]['passwordhash'].encode()
+        if bcrypt.checkpw(password.encode(), password_hashed):
+            return "Success"
+        else:
+            return "Wrong"
 
 
 api.add_resource(ChatAPI, '/api')
 api.add_resource(DataPurge, '/api/deleteData')
+api.add_resource(Register, '/api/register')
+api.add_resource(Login, '/api/login')
 
 if __name__ == "__main__":
     app.run(port=80)
